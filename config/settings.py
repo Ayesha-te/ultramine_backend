bc8ee48198daa1624c56024c57a3167dc075b81f
Ayesha-te <ayeshajahangirrr@gmail.com>
2025-12-22 18:00:14 +0500

    dsfsd

--- config/settings.py
+++ config/settings.py
@@ -66,8 +66,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DATABASE_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': config('DATABASE_NAME', default='neondb'),
+        'USER': config('DATABASE_USER', default='neondb_owner'),
+        'PASSWORD': config('DATABASE_PASSWORD', default=''),
+        'HOST': config('DATABASE_HOST', default=''),
+        'PORT': config('DATABASE_PORT', default='5432'),
+        'CONN_MAX_AGE': 600,
+        'OPTIONS': {
+            'sslmode': 'require',
+        } if config('DATABASE_HOST', default='') else {}
     }
 }
 
